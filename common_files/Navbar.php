25817ae3a4dd99411c962f8db79cc3dd36a4a7d8
ABHIJEET-SRIDHAR-M <abhijeetsridhar14@gmail.com>
2017-01-14 22:56:49 +0530

    Initial Files
    This are the initial folders and file (Only Navbar that is inside
    Comon_files Folder)

---
+++ common_files/Navbar.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Case</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="navbarCssStyling.css">
+  
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+	  <!-- Image pixels - 150 horizontal and 50 vertical-->
+	  <a href="#" class="pull-left"><img src="../images/logo.png"></a>
+      &nbsp &nbsp &nbsp
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#"><span class="glyphicon glyphicon-home">&nbsp </span>Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-th-list">&nbsp </span> Benefits <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#"><span class="glyphicon glyphicon-gift">&nbsp </span>To Students</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-calendar">&nbsp </span>To Schools</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-shopping-cart">&nbsp </span>To Parents</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><span class="glyphicon glyphicon-education">&nbsp </span>School Portal </a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-eye-open">&nbsp </span> Parents Portal </a></li>
+		
+		<li>&nbsp &nbsp <button class="btn btn-danger navbar-btn"><span class="glyphicon glyphicon-phone-alt"> </span> &nbsp We're Hiring</button></li>
+      </ul>
+		<!-- This is for right hand side navbar if required 
+	  <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+	  
+		 Right navbar end -->
+    </div>
+  </div>
+</nav>
+  
+<script src="../bootstrap/jquery/jquery.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
