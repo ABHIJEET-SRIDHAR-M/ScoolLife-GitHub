2ec6c5823c97992fbad2b28aed31831c145db1a4
ABHIJEET-SRIDHAR-M <abhijeetsridhar14@gmail.com>
2017-01-19 12:48:42 +0530

    Improved Image Slider
    1) Added 3 Images in a single image in the carousel.
    2) Added Dummy images for support.
    3) Modified the css accordingly

--- common_files/slider_mp_inc.php
+++ common_files/slider_mp_inc.php
@@ -13,8 +13,10 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
 
-      <div class="item active">
-        <img src="images/img_chania.jpg"> <!-- Include Image 1 here -->
+      <div class="item three-images active">
+        <img src="images/im1.png"> <!-- Include Image 1 here -->
+        <img src="images/im2.png"> <!-- Include Image 1 here -->
+        <img src="images/im3.png"> <!-- Include Image 1 here -->
         <div class="carousel-caption">
           <h3>Chania</h3>
           <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
